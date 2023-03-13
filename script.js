bc03f7a98bdec698d8859c434d4072c46095ec8d
MRinshad <rinshadmorayur09@gmail.com>
2023-03-14 00:08:41 +0530

    nav links linked and some more changes

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-// const showOnPx = 100;
-// const backToTopButton = document.querySelector(".back-to-top")
+const showOnPx = 100;
+const backToTopButton = document.querySelector(".back-to-top")
 
-// const scrollContainer = () => {
-//   return document.documentElement || document.body;
-// };
+const scrollContainer = () => {
+  return document.documentElement || document.body;
+};
 
-// document.addEventListener("scroll", () => {
-//   if (scrollContainer().scrollTop > showOnPx) {
-//     backToTopButton.classList.remove("hidden")
-//   } else {
-//     backToTopButton.classList.add("hidden")
-//   }
-// })
+document.addEventListener("scroll", () => {
+  if (scrollContainer().scrollTop > showOnPx) {
+    backToTopButton.classList.remove("hidden")
+  } else {
+    backToTopButton.classList.add("hidden")
+  }
+})
 
-// const goToTop = () => {
-//     document.body.scrollIntoView();
-//   };
+const goToTop = () => {
+    document.body.scrollIntoView();
+  };
 
-//   backToTopButton.addEventListener("click", goToTop)
+  backToTopButton.addEventListener("click", goToTop)
 
